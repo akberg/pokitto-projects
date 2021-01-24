80340bc14a5ba425f71cd3b060e574c0944564dd
Andreas Berg <andreaskb98@gmail.com>
2021-01-24 18:20:48 +0000

    Add screenshots, remove Pong example (not mine)

--- PongExample/My_settings.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_SETTINGS_H
-#define MY_SETTINGS_H
-
-#define PROJ_HIRES 0                   // Max resolution of 220x176
-//#define PROJ_SCREENMODE MODE_NOBUFFER  // App managed framebuffer
-#define PROJ_ENABLE_SOUND 1            // Sound enabled
-//#define PROJ_ENABLE_SYNTH 1            // Synth enabled
-
-#endif
